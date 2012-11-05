e85cfd7fddb7152122dc435fa337f5a30b6f3888
Milos Soskic <artificermil@gmail.com>
2012-11-05 14:51:52 +0000

    UserTags now only allows user_role of doctors to be associated with Hospital post types

--- plugins/usertags/usertags.php
+++ plugins/usertags/usertags.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: User Tags
 Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
-Description: Enables posts to be "tagged" with registered users of the site enabling WordPress to have multiple users/authors associated with a single post
+Description: Enables posts to be "tagged" with registered users with a particular role thus enabling WordPress to have multiple users/authors associated with a single post
 Version: 0.1
 Author: Milos Soskic (Citrus Mist)
 Author URI: http://citrus-mist.com
@@ -21,10 +21,28 @@ class User_Tags{
 
     function activate() {
     	add_option( 'user_tags_enabled', true);
+		$doctor = add_role( 'doctor', 'Doctor', array(
+			'read' => true,
+			'publish_posts',
+			'delete_posts',
+			'delete_published_posts',
+			'edit_published_posts',
+			'upload_files'
+		));
+		$midwife = add_role( 'midwife', 'Midwife', array(
+			'read' => true,
+			'publish_posts',
+			'delete_posts',
+			'delete_published_posts',
+			'edit_published_posts',
+			'upload_files'
+		));
 	} // end activate
 
 	function deactivate() {
-	    delete_option( 'hide_ajax_notification' );
+		remove_role('doctor');
+		remove_role('midwife');
+	    delete_option( 'user_tags_enabled' );
 	} // end deactivate
 
 	function user_tag_meta_box(){
@@ -47,14 +65,18 @@ class User_Tags{
 
 	function render_meta_box($the_post){
 		$custom = get_post_custom($the_post->ID);
-		log_me($custom);
+		// log_me($custom);
   		$user_tags = isset($custom['user_tags']) ? unserialize($custom['user_tags'][0]) : array();
-		log_me($user_tags);
+		// log_me($user_tags);
 
-    	$assigned_users = get_users(array(
-    			"include" => $user_tags
+		if(empty($user_tags)){
+			$assigned_users = array();
+    	}else{
+	    	$assigned_users = get_users(array(
+    			"include" => $user_tags,
+    			"role"	  => 'doctor'	
     		));
-  		
+    	}
   		// foreach ($user_tags as $user_tag) {
   		// 	$user_tag = json_decode($user_tag);
   		// 	//TODO: instead of calling get_user_by individually we could obtain all the users by passing
@@ -65,7 +87,8 @@ class User_Tags{
   		log_me($assigned_users);
 
     	$avail_users = get_users(array(
-    			'exclude' => $user_tags
+    			'exclude' => $user_tags,
+    			"role"	  => 'doctor'
     		));
   		
     	$labels = get_post_type_object( get_current_screen()->post_type )->labels;
@@ -122,7 +145,8 @@ class User_Tags{
 	        return;
 	    }
 		
-	    if ( !wp_verify_nonce( $_POST["user_tags_nonce"], get_current_theme() ) ){
+	    if (!isset($_POST["user_tags_nonce"]) || 
+	    	!wp_verify_nonce( $_POST["user_tags_nonce"], get_current_theme())){
 	        return;
 	    }
 		
